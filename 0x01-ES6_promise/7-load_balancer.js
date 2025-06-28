af24465d18404fd58c23ded798853ac920fab65f
nati-sahlu <natansahlu5@gmail.com>
2025-06-28 17:12:43 +0300

    task7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,23 +1,3 @@
-import loadBalancer from "./7-load_balancer";
-
-const ukSuccess = 'Downloading from UK is faster';
-const frSuccess = 'Downloading from FR is faster';
-
-const promiseUK = new Promise(function (resolve, reject) {
-  setTimeout(resolve, 100, ukSuccess);
-});
-
-const promiseUKSlow = new Promise(function (resolve, reject) {
-  setTimeout(resolve, 400, ukSuccess);
-});
-
-const promiseFR = new Promise(function (resolve, reject) {
-  setTimeout(resolve, 200, frSuccess);
-});
-
-const test = async () => {
-  console.log(await loadBalancer(promiseUK, promiseFR));      // UK wins
-  console.log(await loadBalancer(promiseUKSlow, promiseFR));  // FR wins
-};
-
-test();
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+}
